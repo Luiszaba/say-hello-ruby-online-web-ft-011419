aaacf66d03f46c89922c85e2431e0c7c7409f099
Luis Zabala <luis.zabala076@gmail.com>
2019-01-14 18:35:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,9 @@
 # Build your say_hello method here
-def say_hello(name = "Gabriela" )
+def say_hello(name)
+  name = "Gabriela"
   puts "Hello #{name}!"
 end
 
-def say_hello ()
+def say_hello (name = "Ruby Programmer")
+  puts "Hello #{name}"
+end
